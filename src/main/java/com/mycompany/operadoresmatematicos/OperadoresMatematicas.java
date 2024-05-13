14e7583791aa2dfa6c1a2fba30f0fcfb55f4bf25
jakit <jakit@DESKTOP-5HTTVKV>
2024-05-13 10:56:01 -0500

    Carga Inicial

---
+++ src/main/java/com/mycompany/operadoresmatematicos/OperadoresMatematicas.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.operadoresmatematicos;
+
+public class OperadoresMatematicas {
+   
+    public static void main(String[] args) {
+
+        // Esto es una prueba para el git, despues lo modificaremos
+       
+        System.out.println("Ingrese Numeros");   
+     
+        
+    }
+    
+}
